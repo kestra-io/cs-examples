5343e5bccc84a41f9d9899c5b3e28193fed4717b
kestra <kestra@kestra-kestra-tech-partners-worker-68ccd6c5dc-qwn26>
2026-08-12 18:50:13 +0200

    adding flows from https://partners.kestra.cloud environment

--- solutions.demo/files/dbt/models/transformed_data.sql
+++
@@ -1,6 +0,0 @@
-select
-  id,
-  cast(created_at AS TIMESTAMP) as timestamp,
-  upper(val),
-  qty
-from {{ ref('raw_data') }}
--- solutions.infra/files/createVM/backend.tf
+++
@@ -1,5 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "kestra-managed-vm-assets"
-  }
-}
